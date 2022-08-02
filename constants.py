654cc6d7ee1781ecbf2916d63945b5689e3a453b
savasoglu <109984658+savasoglu@users.noreply.github.com>
2022-08-02 17:41:40 -0400

    Revert "add files"
    This reverts commit cff6be9b5d0253f125befb2a64c7a7316b0e5c37.

--- constants.py
+++
@@ -1,4 +0,0 @@
-new_canaan_coord = (41.16241, -73.50209)  # (lat, lon)
-
-headache_pressure_range = (1003., 1007.)  # hPa - https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4684554/
-standard_pressure = 1013.25  # hPa - https://en.wikipedia.org/wiki/Atmospheric_pressure
